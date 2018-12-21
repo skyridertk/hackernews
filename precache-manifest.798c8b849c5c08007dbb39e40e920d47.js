426eaa213a3b59d23060509950e1df113cea6373
Ian Kahwai <iankahwai@gmail.com>
2018-12-21 14:24:17 +0200

    Updates

---
+++ precache-manifest.798c8b849c5c08007dbb39e40e920d47.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42264abca56f8de89f7c",
+    "url": "/hackernews/static/css/main.d6031960.chunk.css"
+  },
+  {
+    "revision": "42264abca56f8de89f7c",
+    "url": "/hackernews/static/js/main.42264abc.chunk.js"
+  },
+  {
+    "revision": "90bf941e451e1cf69712",
+    "url": "/hackernews/static/js/1.90bf941e.chunk.js"
+  },
+  {
+    "revision": "da1602332b801076df13",
+    "url": "/hackernews/static/js/runtime~main.da160233.js"
+  },
+  {
+    "revision": "dcecf6cd306b0eb1ef278150bf24c5ce",
+    "url": "/hackernews/index.html"
+  }
+];
